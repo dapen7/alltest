7cc1927b80091deee10fdcf88ae570a4d81e73de
dapen7 <dapen7@126.com>
2020-11-23 23:36:14 +0800

    asd
    asd

--- downloadManager/main.js
+++ downloadManager/main.js
@@ -6,22 +6,73 @@ const filter = {
 }
 
 function createWindow() {
-    session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-        console.log(details);
-    });
+    // session.defaultSession.webRequest.onBeforeRequest(filter, (details, callback) => {
+    //     console.log("onBeforeRequest");
+    //     callback({ requestHeaders: details.requestHeaders });
+    // });
+    // session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+    //     console.log("onBeforeSendHeaders");
+    //     callback({ requestHeaders: details.requestHeaders });
+    // });
+    // session.defaultSession.webRequest.onSendHeaders(filter, (details) => {
+    //     console.log("onSendHeaders");
+    // });
+    // session.defaultSession.webRequest.onHeadersReceived(filter, (details, callback) => {
+    //     console.log("onHeadersReceived");
+    //     callback({ responseHeaders: details.responseHeaders, statusLine: details.statusLine });
+    // });
+    // session.defaultSession.webRequest.onResponseStarted(filter, (details) => {
+    //     console.log("onResponseStarted");
+    // });
+    // session.defaultSession.webRequest.onCompleted(filter, (details) => {
+    //     console.log("onCompleted");
+    // });
+
+    // session.defaultSession.webRequest.onBeforeRedirect(filter, (details) => {
+    //     console.log("onBeforeRedirect");
+    // });
+    // session.defaultSession.webRequest.onErrorOccurred(filter, (details) => {
+    //     console.log("onErrorOccurred");
+    // });
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1920,
+        height: 1080,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation:false
         }
     })
 
-    win.loadFile('src/index.html')
-    win.webContents.openDevTools()
+    // win.loadFile('src/index.html');
+    //https://chromedevtools.github.io/devtools-protocol/tot/Network/
+    //https://www.electronjs.org/docs/api/debugger
+    win.loadURL("http://localhost:3000");
+    win.webContents.openDevTools();
+
+    let requestId;
+    win.webContents.debugger.attach('1.1');
+    win.webContents.debugger.sendCommand('Network.enable');
+    win.webContents.debugger.on('message', (event, method, params) => {
+        if(method === 'Network.requestWillBeSentExtraInfo'){
+            requestId = "";
+        }
+        if (method === 'Network.responseReceived') {
+            if (params.response.url.indexOf('http://localhost:3001') != -1) {
+                requestId = params.requestId;
+                if(requestId){
+                    win.webContents.debugger.sendCommand('Network.getResponseBody', { "requestId": requestId }).then(function({body,base64Encoded}){
+                        console.log(body);
+                    })
+                }
+            }
+        }
+        if(method === 'Network.loadingFailed'){
+            console.log("网络错误:"+params.errorText);
+        }
+    });
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
--- test/client/src/App.vue
+++ test/client/src/App.vue
@@ -1,36 +1,38 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <input
-        class="file"
-        name="file"
-        type="file"
-        @change="update"/>
+    <p>
+        <button @click="test()">test get</button>
+    </p>
+    <p>
+        <input class="file" name="file" type="file" @change="update" />
+    </p>
 </template>
 
 <script>
-import Axios from 'axios';
+import Axios from "axios";
 export default {
     name: "App",
-    components: {
-    },
+    components: {},
     methods: {
-        update(e) {
-
+        test() {
             Axios.get("http://localhost:3001/test").then((response) => {
                 console.log(response.data);
             });
-
-
+        },
+        update(e) {
             let file = e.target.files[0];
             let param = new FormData();
             param.append("myfile", file);
             console.log(param.get("file"));
             let config = {
-                headers: { "Content-Type": "multipart/form-data;charset=UTF-8" },
+                headers: {
+                    "Content-Type": "multipart/form-data;charset=UTF-8",
+                },
             };
-            Axios.post("http://localhost:3001/upload", param, config).then((response) => {
-                console.log(response.data);
-            });
+            Axios.post("http://localhost:3001/upload", param, config).then(
+                (response) => {
+                    console.log(response.data);
+                }
+            );
         },
     },
 };
